aa899b86a798f636b1c9b037e03186a4c9d6a576
Abhay Wankhade <abhaywankhade2004@gmail.com>
2023-09-05 00:17:27 +0530

    this is frontend in tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root'); // Set the root element for accessibility
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [fromEmail, setFromEmail] = useState('');
+  const [recipientEmail, setRecipientEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [text, setText] = useState('');
+  const [emailSent, setEmailSent] = useState(false);
+
+  const handleNextStep = () => {
+    if (step === 1 && fromEmail !== '') {
+      setStep(2);
+    } else if (step === 2 && recipientEmail !== '' && subject !== '' && text !== '') {
+      // Submit the data to the API
+      fetch('https://nodemailer-two-beta.vercel.app/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          from: fromEmail,
+          to: recipientEmail,
+          subject,
+          text,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setEmailSent(true);
+        })
+        .catch((error) => {
+          console.error(error);
+          // Handle error
+        });
+    }
+  };
+
+  const handleReset = () => {
+    setStep(1);
+    setFromEmail('');
+    setRecipientEmail('');
+    setSubject('');
+    setText('');
+    setEmailSent(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+        {step === 1 && (
+          <div className="space-y-8">
+            <div>
+              <label htmlFor="from-email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email (From)</label>
+              <input
+                type="email"
+                id="from-email"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="name@yourdomain.com"
+                required
+                value={fromEmail}
+                onChange={(e) => setFromEmail(e.target.value)}
+              />
+            </div>
+            <button
+              onClick={handleNextStep}
+              className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Next
+            </button>
+          </div>
+        )}
+        {step === 2 && (
+          <div>
+            {emailSent ? (
+              <div className="text-center">
+                <p className="mb-4 text-lg font-medium text-green-500">Email sent successfully!</p>
+                <button
+                  onClick={handleReset}
+                  className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Go back to Step 2
+                </button>
+              </div>
+            ) : (
+              <form>
+                <div>
+                  <label htmlFor="recipient-email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Recipient's email (To)</label>
+                  <input
+                    type="email"
+                    id="recipient-email"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    placeholder="recipient@example.com"
+                    required
+                    value={recipientEmail}
+                    onChange={(e) => setRecipientEmail(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+                  <input
+                    type="text"
+                    id="subject"
+                    className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    placeholder="Let us know how we can help you"
+                    required
+                    value={subject}
+                    onChange={(e) => setSubject(e.target.value)}
+                  />
+                </div>
+                <div className="sm:col-span-2">
+                  <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                  <textarea
+                    id="message"
+                    rows="6"
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="Leave a comment..."
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                  ></textarea>
+                </div>
+                <button
+                  onClick={handleNextStep}
+                  className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Send message
+                </button>
+              </form>
+            )}
+          </div>
+        )}
+      </div>
+    </section>
   );
 }
 
